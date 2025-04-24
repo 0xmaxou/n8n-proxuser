76dd8fd578c87bdd78e89b46531ba88cddced678
0xmaxou <maxsouv80@gmail.com>
2025-04-24 18:44:48 +0200

    Update proxy-n8n.js

--- api/proxy-n8n.js
+++ api/proxy-n8n.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     return res.status(405).send("Method not allowed");
   }
 
-  const webhookURL = "https://maxsurf34.app.n8n.cloud/webhook-test/a963e223-ec8f-4a82-801b-3748e5b7d458"; // remplace ici !
+  const webhookURL = "https://maxsurf34.app.n8n.cloud/webhook/a963e223-ec8f-4a82-801b-3748e5b7d458"; // remplace ici !
 
   try {
     const response = await fetch(webhookURL, {
